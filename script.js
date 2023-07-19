314c1256302121e42d544b42415bc3ca13e62d60
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-07-19 04:51:18 +0200

    Add script from previous lesson

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Test");
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    readStatus = () => read ? "already read" : "not read yet";
+
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${readStatus()}`;
+    }
+}
+
+// const HarryPotter1 = new Book("Harry Potter: Erstes Buch", "Chrissi Rowling", 254, false);
+// console.log(HarryPotter1.info());
